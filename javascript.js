2b42e9bc3af55d21e88763a1ba6697996b0a70e6
gdgeneva2007-tech <251467665+gdgeneva2007-tech@users.noreply.github.com>
2026-01-30 12:44:04 +0800

    Add UI and event triggers
    1.In JavaScript,most of web development is driven by events,not loops.Loops failed in this program for the timeline reason.
    2.In this project,use the 'click' events to trigger 'playRound', and use a function to check the winner.remove the loop entirely.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
+//const { createElement } = require("react");
+
+const button1=document.createElement("button");
+const button2=document.createElement("button");
+const button3=document.createElement("button");
+const body=document.querySelector("body");
+button1.textContent="ROCK";
+button2.textContent="SCISSORS";
+button3.textContent="PAPER";
+body.appendChild(button1);
+body.appendChild(button2);
+body.appendChild(button3);
 let humanScore=0;
 let computerScore=0;
 let computerChoice;
 let humanChoice;
+
 function getComputerChoice(){
     let compu=(Math.floor(Math.random()*10))%3;
     if(compu==0){
@@ -15,56 +28,95 @@ function getComputerChoice(){
         return "PAPER";
     }
 }
-function getHumanChoice(){
-    let human=prompt("What is your choice?");
-    return human.toUpperCase();
-}
+
+
+
+
 
 
 function playRound(humanChoice,computerChoice){
-    
+    const div=document.createElement("div");
+    const p1=document.createElement("p");
+    const p2=document.createElement("p");
+    p1.textContent="ROUND: computerChoice:"+computerChoice+" humanChoice"+humanChoice;
     console.log("ROUND");
     console.log(computerChoice);
     console.log(humanChoice);
     if(humanChoice==computerChoice){
         console.log("It is a tie!");
+        p2.textContent="It is a tie!";
     }
     else if(humanChoice=="ROCK"&&computerChoice=="SCISSORS"){
         console.log("You win! Rock beats Scissors");
+        p2.textContent="You win! Rock beats Scissors";
         humanScore++;
     }
     else if(humanChoice=="ROCK"&&computerChoice=="PAPER"){
         console.log("You lose! Paper beats Rock");
+        p2.textContent="You lose! Paper beats Rock";
         computerScore++;
     }
     else if(humanChoice=="SCISSORS"&&computerChoice=="PAPER"){
         console.log("You win! Scissors beats Paper");
+        p2.textContent="You win! Scissors beats Paper";
         humanScore++;
     }
     else if(humanChoice=="SCISSORS"&&computerChoice=="ROCK"){
         console.log("You lose! Rock beats Scissors");
+        p2.textContent="You lose! Rock beats Scissors";
         computerScore++;
     }
     else if(humanChoice=="PAPER"&&computerChoice=="ROCK"){
         console.log("You win! Paper beats Rock");
+        p2.textContent="You win! Paper beats Rock";
         humanScore++;
     }
     else if(humanChoice=="PAPER"&&computerChoice=="SCISSORS"){
         console.log("You lose! Scissors beats Paper");
+        p2.textContent="You lose! Scissors beats Paper";
         computerScore++;
+    }
+    div.appendChild(p1);
+    div.appendChild(p2);
+    body.appendChild(div);
+}
+
+button1.addEventListener("click",()=>{
+    computerChoice=getComputerChoice();
+    playRound("ROCK",computerChoice);
+    checkScore();
+});
+button2.addEventListener("click",()=>{
+    computerChoice=getComputerChoice();
+    playRound("SCISSORS",computerChoice);
+    checkScore();
+});
+button3.addEventListener("click",()=>{
+    computerChoice=getComputerChoice();
+    playRound("PAPER",computerChoice);
+    checkScore();
+});
+function checkScore(){
+    if(humanScore==5){
+        const p3=document.createElement('p');
+        p3.textContent="GAMEOVERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR    humanScore: "+humanScore+" computerScore: "+computerScore+" You win!";
+        body.appendChild(p3);
+        button1.disabled=true;
+        button2.disabled=true;
+        button3.disabled=true;
+    }
+    else if(computerScore==5){
+        const p3=document.createElement('p');
+        p3.textContent="GAMEOVERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR    humanScore: "+humanScore+" computerScore: "+computerScore+" You lose!";
+        body.appendChild(p3);
+        button1.disabled=true;
+        button2.disabled=true;
+        button3.disabled=true;
     }
 }
 
-for(let i=1;i<=5;i++){
-    computerChoice=getComputerChoice();
-    humanChoice=getHumanChoice();
-    playRound(humanChoice,computerChoice)
-}
-console.log(humanScore);
-console.log(computerScore);
-if(humanScore>computerScore){
-    console.log("You win!");
-}
-else{
-    console.log("You lose!")
-}
+
+
+
+
+
